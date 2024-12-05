091d6f1ed0fddce051d771a8d63840622dc11b4d
Kevin E. Ford <4380198+Bowman74@users.noreply.github.com.>
2024-12-05 13:58:00 -0700

    Time Picker bug

---
+++ TimePickerBug/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace TimePickerBug
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private bool _timeSelectedChanging = false;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+
+        private TimeSpan _StartTime;
+
+        public TimeSpan StartTime
+        {
+            get { return _StartTime; }
+            set
+            {
+                if (TimeSpan.Compare(_StartTime, value) != 0)
+                {
+                    _StartTime = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private void TimePicker_TimeSelected(object sender, TimeChangedEventArgs e)
+        {
+            if (_timeSelectedChanging)
+            {
+                return;
+            }
+            var timeDifference = e.NewTime - e.OldTime;
+
+            _timeSelectedChanging = true;
+            this.StartTime = e.OldTime;
+        }
+    }
+
+}
